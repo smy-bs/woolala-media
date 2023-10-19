ef98c2ebfee8f62f0fe3cd1f861e01bf5f7190af
Miyoung SB <bs.miyoung@gmail.com>
2023-10-19 15:36:36 +0200

    javascript Up date

--- include/js/main.js
+++ include/js/main.js
@@ -11,25 +11,17 @@ document.addEventListener("scroll", () => {
   }
 });
 
-// navbar mobile version
-
 //Handle scrolling when tapping on the navbar menu
+
+const navToggle = document.querySelector(".navbar__toggle-btn");
 const navbarMenu = document.querySelector(".nav_menu");
-navbarMenu.addEventListener("click", (event) => {
-  const target = event.target;
-  const link = target.dataset.link;
-  if (link == null) {
-    return;
-  }
+navToggle.addEventListener("click", () => {
+     navbarMenu.classList.toggle("open");
+     });
+navbarMenu.addEventListener("click", ()=>{
   navbarMenu.classList.remove("open");
-  scrollIntoView(link);
 });
 
-//Navbar toggle button for small screen - not function!
-const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
-navbarToggleBtn.addEventListener("click", () => {
-  navbarMenu.classList.toggle("open");
-});
 
 // Handle click on "télécharge" button on home
 const homeTelechargeBtn = document.querySelector(".telecharge");
@@ -37,12 +29,6 @@ homeTelechargeBtn.addEventListener("click", () => {
   scrollIntoView("#contents");
 });
 
-//transparent
-const home = document.querySelector(".home_container");
-const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener("scroll", () => {
-  home.style.opacity = 1 - window.scrollY / homeHeight;
-});
 
 //Show " arrow up" button when scrolling down
 const arrowUp = document.querySelector(".arrow-up");
